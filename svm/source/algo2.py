6997ed720d90163bf026af59f5f7399acb06b201
kevinn-ig <148980240+kevinn-ig@users.noreply.github.com>
2023-12-08 13:42:08 -0500

    Add files via upload

--- svm/source/algo2.py
+++ svm/source/algo2.py
@@ -1,52 +1,32 @@
+import os
 import pandas as pd
+import numpy as np
 from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import make_scorer, recall_score, f1_score, accuracy_score
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.model_selection import GridSearchCV, train_test_split
 
-# Load the data
-df_train = pd.read_csv("/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/gtzan/Data/features_30_sec.csv")
-df_test_2_1 = pd.read_csv("/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/2:1.csv")
-df_test_10_1 = pd.read_csv("/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/10:1.csv")
+def load_data():
+    # Load the data
+    df_train = pd.read_csv("/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/gtzan/Data/features_30_sec.csv")
+    df_test_2_1 = pd.read_csv("/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/2:1.csv")
+    df_test_10_1 = pd.read_csv("/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/10:1.csv")
+    return df_train, df_test_2_1, df_test_10_1
 
-# Define the specific features you want to use for testing
-selected_features = [
-    'mfcc6_mean', 'mfcc3_mean', 'chroma_stft_var', 'mfcc5_var',
-    'mfcc4_var', 'mfcc4_mean', 'mfcc1_mean', 'rms_mean',
-    'mfcc19_mean', 'mfcc6_var'
-]
+def select_features(df_train, selected_features):
+    X_train, X_val, y_train, y_val = train_test_split(df_train[selected_features], df_train['label'], test_size=0.05, random_state=42)
+    return X_train, X_val, y_train, y_val
 
-# Extract only the selected features and the label for training
-X_train, X_val, y_train, y_val = train_test_split(df_train[selected_features], df_train['label'], test_size=0.05, random_state=42)
+def standardize_features(X_train, X_val, X_test_2_1, X_test_10_1):
+    scaler = MinMaxScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
+    X_test_2_1_scaled = scaler.transform(X_test_2_1)
+    X_test_10_1_scaled = scaler.transform(X_test_10_1)
+    return X_train_scaled, X_val_scaled, X_test_2_1_scaled, X_test_10_1_scaled
 
-# Extract only the selected features and the label for testing (2:1 ratio)
-X_test_2_1 = df_test_2_1[selected_features]
-y_test_2_1 = df_test_2_1['genre']
-
-# Extract only the selected features and the label for testing (10:1 ratio)
-X_test_10_1 = df_test_10_1[selected_features]
-y_test_10_1 = df_test_10_1['genre']
-
-# Standardize the features using MinMaxScaler
-scaler = MinMaxScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-X_val_scaled = scaler.transform(X_val)
-X_test_2_1_scaled = scaler.transform(X_test_2_1)
-X_test_10_1_scaled = scaler.transform(X_test_10_1)
-genre_accuracies = {}  # Dictionary to store accuracies for each genre
-
-
-
-# Number of iterations for averaging
-num_iterations = 10
-results_list = []
-
-for iteration in range(num_iterations):
-    # Print the selected feature names
-    print(f"\nIteration {iteration + 1} - Selected Features:")
-    print(X_train.columns)
-
+def tune_svm_model(X_train_scaled, y_train):
     # Compute class weights to handle class imbalance
     class_weights = compute_class_weight('balanced', classes=pd.unique(y_train), y=y_train)
     class_weight_dict = dict(zip(pd.unique(y_train), class_weights))
@@ -68,49 +48,30 @@ for iteration in range(num_iterations):
 
     # Extract the best model
     best_svm_model = grid_search.best_estimator_
+    best_hyperparameters = grid_search.best_params_
 
-     # Make predictions on the validation set
-    y_pred_val = best_svm_model.predict(X_val_scaled)
+    return best_svm_model, best_hyperparameters
 
-    # Calculate accuracy for each genre in the validation set
-    for genre in df_train['label'].unique():
-        genre_indices = y_val == genre
-        genre_accuracy = accuracy_score(y_val[genre_indices], y_pred_val[genre_indices])
-        
-        if genre not in genre_accuracies:
-            genre_accuracies[genre] = []
-        
-        genre_accuracies[genre].append(genre_accuracy)
-
-
+def evaluate_model(model, X_val_scaled, y_val, X_test_2_1_scaled, y_test_2_1, X_test_10_1_scaled, y_test_10_1):
     # Evaluate the model performance on the validation set
+    y_pred_val = model.predict(X_val_scaled)
     recall_val = recall_score(y_val, y_pred_val, average='weighted')
     f1_val = f1_score(y_val, y_pred_val, average='weighted')
     accuracy_val = accuracy_score(y_val, y_pred_val)
 
     # Make predictions on the test set (2:1 ratio)
-    y_pred_2_1 = best_svm_model.predict(X_test_2_1_scaled)
-
-    # Evaluate the model performance (2:1 ratio)
+    y_pred_2_1 = model.predict(X_test_2_1_scaled)
     recall_2_1 = recall_score(y_test_2_1, y_pred_2_1, average='weighted')
     f1_2_1 = f1_score(y_test_2_1, y_pred_2_1, average='weighted')
     accuracy_2_1 = accuracy_score(y_test_2_1, y_pred_2_1)
 
     # Make predictions on the test set (10:1 ratio)
-    y_pred_10_1 = best_svm_model.predict(X_test_10_1_scaled)
-
-    # Evaluate the model performance (10:1 ratio)
+    y_pred_10_1 = model.predict(X_test_10_1_scaled)
     recall_10_1 = recall_score(y_test_10_1, y_pred_10_1, average='weighted')
     f1_10_1 = f1_score(y_test_10_1, y_pred_10_1, average='weighted')
     accuracy_10_1 = accuracy_score(y_test_10_1, y_pred_10_1)
 
-    # Extract the best hyperparameters as a dictionary
-    best_hyperparameters = grid_search.best_params_
-
-    # Store the results in a dictionary
-    results_dict = {
-        'Iteration': iteration + 1,
-        'Best Hyperparameters': str(best_hyperparameters),
+    return {
         'Recall (Validation Set)': recall_val,
         'F1 Score (Validation Set)': f1_val,
         'Accuracy (Validation Set)': accuracy_val,
@@ -122,25 +83,101 @@ for iteration in range(num_iterations):
         'Accuracy (10:1 ratio)': accuracy_10_1
     }
 
-    results_list.append(results_dict)
+def genre_wise_evaluation(model, X_val_scaled, y_val, X_test_2_1_scaled, y_test_2_1, X_test_10_1_scaled, y_test_10_1):
+    genre_results = {}
 
-# Calculate average accuracy for each genre
-average_genre_accuracies = {genre: sum(accuracies) / num_iterations for genre, accuracies in genre_accuracies.items()}
+    # Genre-wise evaluation for the validation set
+    for genre in pd.unique(y_val):
+        genre_indices = y_val == genre
+        y_pred_val_genre = model.predict(X_val_scaled[genre_indices])
+        genre_accuracy_val = accuracy_score(y_val[genre_indices], y_pred_val_genre)
 
-# Print average accuracy for each genre
-print("\nAverage Accuracy for Each Genre:")
-for genre, accuracy in average_genre_accuracies.items():
-    print(f"{genre}: {accuracy}")
+        genre_results[genre] = {
+            'Accuracy (Validation Set)': genre_accuracy_val,
+            'Iterations': []
+        }
 
-# Convert the results to a DataFrame
-results_df = pd.DataFrame(results_list)
+    # Genre-wise evaluation for the test set (2:1 ratio)
+    for genre in pd.unique(y_test_2_1):
+        genre_indices_2_1 = y_test_2_1 == genre
+        y_pred_2_1_genre = model.predict(X_test_2_1_scaled[genre_indices_2_1])
+        genre_accuracy_2_1 = accuracy_score(y_test_2_1[genre_indices_2_1], y_pred_2_1_genre)
 
-# Save the results to a CSV file
-results_csv_path = '/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/results/results_average.csv'
-results_df.to_csv(results_csv_path, index=False)
+        genre_results[genre]['Iterations'].append({
+            'Accuracy (2:1 ratio)': genre_accuracy_2_1
+        })
 
-# Display the average results (excluding 'Best Hyperparameters' column)
-average_results = results_df.drop(['Iteration', 'Best Hyperparameters'], axis=1).mean()
-print("\nAverage Results:")
-print(average_results)
-print(f"Results saved to: {results_csv_path}")
+    # Genre-wise evaluation for the test set (10:1 ratio)
+    for genre in pd.unique(y_test_10_1):
+        genre_indices_10_1 = y_test_10_1 == genre
+        y_pred_10_1_genre = model.predict(X_test_10_1_scaled[genre_indices_10_1])
+        genre_accuracy_10_1 = accuracy_score(y_test_10_1[genre_indices_10_1], y_pred_10_1_genre)
+
+        genre_results[genre]['Iterations'].append({
+            'Accuracy (10:1 ratio)': genre_accuracy_10_1
+        })
+
+    return genre_results
+
+def main():
+    df_train, df_test_2_1, df_test_10_1 = load_data()
+
+    # Define the specific features you want to use for testing
+    selected_features = [
+        'mfcc6_mean', 'mfcc3_mean', 'chroma_stft_var', 'mfcc5_var',
+        'mfcc4_var', 'mfcc4_mean', 'mfcc1_mean', 'rms_mean',
+        'mfcc19_mean', 'mfcc6_var'
+    ]
+
+    X_train, X_val, y_train, y_val = select_features(df_train, selected_features)
+
+    X_test_2_1 = df_test_2_1[selected_features]
+    y_test_2_1 = df_test_2_1['genre']
+
+    X_test_10_1 = df_test_10_1[selected_features]
+    y_test_10_1 = df_test_10_1['genre']
+
+    X_train_scaled, X_val_scaled, X_test_2_1_scaled, X_test_10_1_scaled = standardize_features(
+        X_train, X_val, X_test_2_1, X_test_10_1
+    )
+
+    # Number of iterations for averaging
+    num_iterations = 10
+    results_list = []
+    genre_results_list = []
+
+    for iteration in range(num_iterations):
+        # Print the selected feature names
+        print(f"\nIteration {iteration + 1} - Selected Features:")
+        print(X_train.columns)
+
+        best_svm_model, best_hyperparameters = tune_svm_model(X_train_scaled, y_train)
+
+        # Evaluate the model
+        evaluation_results = evaluate_model(best_svm_model, X_val_scaled, y_val, X_test_2_1_scaled, y_test_2_1, X_test_10_1_scaled, y_test_10_1)
+        results_list.append({'Iteration': iteration + 1, 'Best Hyperparameters': best_hyperparameters, **evaluation_results})
+
+        # Genre-wise evaluation
+        genre_results = genre_wise_evaluation(best_svm_model, X_val_scaled, y_val, X_test_2_1_scaled, y_test_2_1, X_test_10_1_scaled, y_test_10_1)
+        genre_results_list.append({'Iteration': iteration + 1, 'Genre Results': genre_results})
+
+    # Convert the results to DataFrames
+    results_df = pd.DataFrame(results_list)
+    genre_results_df = pd.DataFrame(genre_results_list)
+
+    # Save the results to CSV files
+    results_csv_path = '/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/results/results_average.csv'
+    genre_results_csv_path = '/Users/kevin_smith/Desktop/FSU_Relevant_Stuff/fall_2023/CAP5771/project/results/genre_results_average.csv'
+
+    results_df.to_csv(results_csv_path, index=False)
+    genre_results_df.to_csv(genre_results_csv_path, index=False)
+
+    # Display the average results (excluding 'Best Hyperparameters' column)
+    average_results = results_df.drop(['Iteration', 'Best Hyperparameters'], axis=1).mean()
+    print("\nAverage Results:")
+    print(average_results)
+    print(f"Results saved to: {results_csv_path}")
+    print(f"Genre-wise results saved to: {genre_results_csv_path}")
+
+if __name__ == "__main__":
+    main()
